e18781c3bccfb7d2d67ed3bb8a3c462452f8c13a
Pavel Terentev <p.terentev@sgnl.pro>
2025-10-29 21:00:08 +0700

    app: get rid of notification logic

--- preload.js
+++ preload.js
@@ -1,30 +1,3 @@
-const { ipcRenderer } = require('electron');
-
-(function patchNotifications() {
-  const NativeNotification = window.Notification;
-
-  // Always report permission as granted
-  Object.defineProperty(window.Notification, 'permission', {
-    get: () => 'granted',
-  });
-  window.Notification.requestPermission = async () => 'granted';
-
-  // Proxy constructor
-  function ProxyNotification(title, options = {}) {
-    try {
-      // forward to main to show a native notification and update tray/badges
-      ipcRenderer.send('notify:show', { title, options });
-
-      // optionally still create real renderer-side notification (for in-page behavior)
-      return new NativeNotification(title, options);
-    } catch {
-      return new NativeNotification(title, options);
-    }
-  }
-  ProxyNotification.prototype = NativeNotification.prototype;
-  ProxyNotification.permission = 'granted';
-  ProxyNotification.requestPermission = window.Notification.requestPermission;
-
-  // Replace global Notification
-  window.Notification = ProxyNotification;
-})();
+// Intentionally left minimal.
+// We don't override Notification to let the site behave like a browser.
+// Keep this file to preserve contextIsolation boundary if we need to expose safe APIs later.
